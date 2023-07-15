238f74f4cc5924a072fe2e2426853841194e65ab
nicksolony <nicksolony@gmail.com>
2023-07-15 01:30:04 -0400

    started working homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,15 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+let galleryContainer = document.querySelector('ul.gallery');
+
+console.log(galleryContainer);
+
+let galleryElements = galleryItems.map(({preview, original, description}) => {
+     return `<img 
+                src="${preview}" alt="${description}" width="320px class"
+            >`
+}).join(' ');
+
+galleryContainer.innerHTML = galleryElements;
