e5a5d0679b2c8229aec7e3b804f07e741ca82b09
nicksolony <nicksolony@gmail.com>
2023-07-17 20:50:05 -0400

    created gallery markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,16 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 let galleryContainer = document.querySelector('ul.gallery');
 
 console.log(galleryContainer);
 
 let galleryElements = galleryItems.map(({preview, original, description}) => {
-     return `<img 
-                src="${preview}" alt="${description}" width="320px class"
-            >`
+     return `<li class="gallery__item" data-source = ${original}>
+                <img
+                    src="${preview}" alt="${description}" class="gallery__image"
+                >
+            </li>`
 }).join(' ');
 
 galleryContainer.innerHTML = galleryElements;
